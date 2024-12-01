a3c442ab051298da25bb4f0ee036c9370cf301c3
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-12-01 15:50:58 -0500

    Add EnableConfigurationProperties to main app context

--- src/main/kotlin/com/ispy/ispy/IspyApplication.kt
+++ src/main/kotlin/com/ispy/ispy/IspyApplication.kt
@@ -1,9 +1,11 @@
 package com.ispy.ispy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.ispy.ispy"])
+@EnableConfigurationProperties
 class IspyApplication
 
 fun main(args: Array<String>) {
