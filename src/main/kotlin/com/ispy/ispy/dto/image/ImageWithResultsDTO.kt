b60272e262df8ac600bde93327b7cfb45180fa5a
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-12-10 08:59:24 -0500

    add new dto

---
+++ src/main/kotlin/com/ispy/ispy/dto/image/ImageWithResultsDTO.kt
@@ -0,0 +1,17 @@
+package com.ispy.ispy.dto.image
+
+import com.ispy.ispy.dto.image.algos.*
+import com.ispy.spy.jooq.tables.*
+
+
+import kotlin.Metadata
+
+data class ImageWithResultsDTO(
+    val image: ImageDTO,
+    val elaResults: List<ElaDTO>? = emptyList(),
+    val noiseAnalysisResults: List<NoiseAnalysisDTO>? = emptyList(),
+    val metadata: List<MetadataDTO>? = emptyList(),
+    val historicalWeather: List<HistoricalWeatherDTO>? = emptyList(),
+    val deepLearningWeather: List<DeepLearningWeatherDTO>? = emptyList(),
+    val geolocation: List<GeolocationDTO>? = emptyList(),
+)
